8faf753541f7b273e53e16c924c43cbcfe7a5097
Anton <nikonov.web@gmail.com>
2023-10-11 14:08:20 +0300

    Add php code sniffer

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,8 @@ namespace Cli;
 use function cli\line;
 use function cli\prompt;
 
-function gamestart () {
+function gamestart()
+{
 line('Welcome to the Brain Game!');
 $name = prompt('May I have your name?');
 line("Hello, %s!", $name);
