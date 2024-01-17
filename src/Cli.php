018ee3f2ae3a98f5ccbce3c710628977f5d0e051
AnNikonov <nikonov.web@gmail.com>
2024-01-17 17:37:03 +0300

    output bug fix

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,10 @@ namespace Cli;
 use function cli\line;
 use function cli\prompt;
 
-function gameStart(): void
+function gameStart()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
